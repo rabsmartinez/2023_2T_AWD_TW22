f8eff5ca1e75abc3f1968efa175f4c0f8d687ed3
rabsmartinez <156283279+rabsmartinez@users.noreply.github.com>
2024-02-15 20:02:09 +0800

    Add files via upload

---
+++ S3-Technical/script.js
@@ -0,0 +1,28 @@
+let currentInput = '';
+
+function appendToDisplay(value) {
+    currentInput += value;
+    document.getElementById('display').value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    document.getElementById('display').value = '';
+}
+
+function operation(operator) {
+    if (currentInput !== '') {
+        currentInput += operator;
+        document.getElementById('display').value = currentInput;
+    }
+}
+
+function calculateResult() {
+    try {
+        const result = eval(currentInput);
+        document.getElementById('display').value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
